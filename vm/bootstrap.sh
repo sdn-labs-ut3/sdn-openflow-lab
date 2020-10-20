cca8bad6a5ea3f1ce89c40a443d08c875e0417b5
Emmanuel Lavinal <Emmanuel.Lavinal@univ-tlse3.fr>
2020-10-21 00:13:09 +0200

    Updating VM bootstrap

--- vm/bootstrap.sh
+++ vm/bootstrap.sh
@@ -10,20 +10,17 @@ sudo apt-get install -y --no-install-recommends --fix-missing\
   openvswitch-switch \
   python3-pip \
   python3-virtualenv \
+  virtualenv \
   xauth \
   xterm
 
-# ---- Install virtualenv ----
-# pip3 install virtualenv
-
 # ---- Install RYU ----
-virtualenv ryuenv
+virtualenv -p /usr/bin/python3 ryuenv
 source ~/ryuenv/bin/activate
 pip install ryu
 deactivate
 
 # ---- Setup folder lab ----
-mkdir ~/tpsdn
-cp ~/ryuenv/lib/python3.6/site-packages/ryu/app/simple_switch_13.py ~/tpsdn/
+git clone https://github.com/elavinal/sdn-openflow-lab.git
 
 echo "**** DONE PROVISIONING VM ****"
